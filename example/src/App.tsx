25bdb99624549283d35308a4ac787d525e8289f9
MichelYAM <michel.yam92@gmail.com>
2023-02-17 16:15:21 +0100

    style fix calendar

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,24 +1,112 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
+
+import { Datapicker } from 'my-react-datapicker'
+import 'my-react-datapicker/dist/stylesheets/datapicker.css'
 import './App.css';
 
 function App() {
+  const [startDate, setStartDate] = useState("")
+  const [start, setStart] = useState("")
+
+  const getYear = (date: Date) => {
+    return date.getFullYear()
+  }
+
+  const range = (start: number, end: number) => {
+    const year = [];
+    for (let i = start; i <= end; i++) {
+      year.push(i);
+    }
+    return year;
+  }
+  const years = range(1990, getYear(new Date()) + 10);
+  const months = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Datapicker dataFormat='DD/MM/YYYY' selectedDate={startDate} setSelectedDate={setStartDate}
+        customHeader={({
+          currentMonth,
+          currentYear,
+          changeMonth,
+          changeYear,
+          prev,
+          next,
+        }) => (
+          <div>
+            <button onClick={prev}>
+              {"<"}
+            </button>
+            <select
+              value={months[currentMonth as unknown as number]}
+              onChange={({ target: { value } }) =>
+                changeMonth(months.indexOf(value))
+              }
+            >
+              {months.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+            <select
+              value={currentYear}
+              onChange={({ target: { value } }: any) => changeYear(value)}
+            >
+              {years.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+            <button onClick={next} >
+              {">"}
+            </button>
+          </div>
+        )}
+      />
+      <Datapicker dataFormat='DD/MM/YYYY' selectedDate={start} setSelectedDate={setStart}
+        customHeader={({
+          currentMonth,
+          currentYear,
+          changeMonth,
+          changeYear,
+          prev,
+          next,
+        }) => (
+          <div>
+            <button onClick={prev}>
+              {"<"}
+            </button>
+            <select
+              value={months[currentMonth as unknown as number]}
+              onChange={({ target: { value } }) =>
+                changeMonth(months.indexOf(value))
+              }
+            >
+              {months.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+            <select
+              value={currentYear}
+              onChange={({ target: { value } }: any) => changeYear(value)}
+            >
+              {years.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+            <button onClick={next} >
+              {">"}
+            </button>
+          </div>
+        )}
+      />
     </div>
   );
 }
